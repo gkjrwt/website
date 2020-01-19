d59f37922f6d736d0525cb7a934031e5daa8566a
ahmedali7o1 <bluemax7o1@gmail.com>
2020-01-20 01:22:20 +0200

    move to jekyll

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ng build --prod &&\
-  rm -rf ../nodearch.github.io/* &&\
-  cp -rf dist/website/* ../nodearch.github.io/ &&\
-  cp src/404.html ../nodearch.github.io/ &&\
-  cp CNAME ../nodearch.github.io/
--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  docsServer: {
-    versionList: 'https://nodearch.io/documentation/{language}/version-list.json',
-    versionGroup: 'https://nodearch.io/documentation/{language}/{version}.json',
-    docsList: 'https://nodearch.io/documentation/docs-list.json',
-    docsBaseUrl: 'https://nodearch.io/documentation'
-  }
-};
